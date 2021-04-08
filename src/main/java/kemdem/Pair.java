63ebdb5dc74f4cd99652650484edbd8cf9dc68b6
Neil Madden <neil.madden@forgerock.com>
2021-04-08 15:32:09 +0100

    Remove incorrect copyright headers

--- src/main/java/kemdem/Pair.java
+++ src/main/java/kemdem/Pair.java
@@ -1,11 +1,3 @@
-/*
- * Copyright 2021 ForgeRock AS. All Rights Reserved
- *
- * Use of this code requires a commercial software license with ForgeRock AS.
- * or with one of its affiliates. All use shall be exclusively subject
- * to such license between the licensee and ForgeRock AS.
- */
-
 package kemdem;
 
 /**
